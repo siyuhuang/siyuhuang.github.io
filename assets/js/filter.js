194d3b0258ace24f2a1327cd949d88e7d9f60dd7
Siyu Huang <30685235+siyuhuang@users.noreply.github.com>
2025-07-06 15:41:02 -0400

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,4 +1,3 @@
-// --- 这是净化过的代码，请复制这个版本 ---
 $(document).ready(function() {
     // 监听所有class为 .filter-btn 的按钮的点击事件
     $('.filter-btn').on('click', function(e) {
@@ -16,13 +15,13 @@ $(document).ready(function() {
 
         // --- 执行筛选逻辑 ---
         if (filterValue === 'all') {
-            // 如果点击的是 "All"，则显示所有出版物
-            $('.pub-item').show(200);
+            // 如果点击的是 "All"，则立即显示所有出版物
+            $('.pub-item').show();
         } else {
-            // 否则，先隐藏所有出版物
-            $('.pub-item').hide(200);
-            // 然后只显示那些class中包含 filterValue 的出版物
-            $('.pub-item.' + filterValue).show(200);
+            // 否则，立即隐藏所有出版物
+            $('.pub-item').hide();
+            // 然后立即显示那些class中包含 filterValue 的出版物
+            $('.pub-item.' + filterValue).show();
         }
     });
 });
